a02f4d743741fc6f4573cd88e484e068e3a419b0
shriyashpachpande <shriyashpachpande813@gmail.com>
2025-11-14 20:42:45 +0530

    upload

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'; // Adjust if deployed elsewhere
+const BASE_URL = 'https://nearbyhospitals-gvrd.onrender.com'; // Adjust if deployed elsewhere
 
 export const getNearbyHospitals = async (lat, lng) => {
   const response = await axios.get(`${BASE_URL}/search-hospitals`, {
